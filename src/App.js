cf04976adf21e5c53f5c00ccd024e70c39773de1
Anuradha-k-p <swapnalipawar587@gmail.com>
2024-01-22 22:46:36 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Links from "./NavLinks/Links";
+
+function App() {
+  return (
+    <div className="App">
+      <div id="title">
+        {/* <div className="the">The</div> <div className="siren"> Siren</div> */}
+        <h2> THE SIREN</h2>
+      </div>
+      <Links/>
+    </div>
+  );
+}
+
+export default App;
